027965205e81fd6ce97015c50f90a4a6530b19da
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-07-29 22:15:56 +1000

    [package.json,package-lock.json] Upgraded dependencies; bump version; [index.d.ts] Regenerate

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,9 @@
 import * as restify from 'restify';
-import { ErrorVar, JsonSchema, MultiResult } from 'tv4';
+import { JsonSchema, MultiResult } from 'tv4';
+import { CustomJsonError } from './interfaces.d';
 export declare const has_body: (req: restify.Request, res: restify.Response, next: restify.Next) => void;
-export declare const jsonSchemaErrorParser: (body_is: MultiResult) => import("./interfaces").IErrorMsg | (import("./interfaces").IErrorMsg & {
-    error_metadata: {};
-}) | (import("./interfaces").IErrorMsg & {
-    error_metadata: {};
-} & ErrorVar) | undefined;
+export declare const jsonSchemaErrorParser: (body_is: MultiResult) => CustomJsonError | undefined;
 export declare const mk_valid_body_mw: (json_schema: JsonSchema, to_res?: boolean) => (request: restify.Request, res: restify.Response, next: restify.Next) => void;
 export declare const mk_valid_body_mw_ignore: (json_schema: JsonSchema, ignore: string[]) => (req: restify.Request, res: restify.Response, next: restify.Next) => void;
-export declare const jsonSchemaNamedArrayOf: (json_schema: JsonSchema, type_name?: string | undefined, type_plural?: string | undefined) => JsonSchema;
+export declare const jsonSchemaNamedArrayOf: (json_schema: tv4.JsonSchema, type_name?: string | undefined, type_plural?: string | undefined) => tv4.JsonSchema;
 export declare const remove_from_body: (keys: string[]) => (request: restify.Request, res: restify.Response, next: restify.Next) => void;
