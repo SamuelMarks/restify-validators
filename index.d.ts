92d0cf91781a18634e23bc683b8ba7591869ca70
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-06-01 10:37:59 -0400

    [{package,package-lock}.json] Upgrade dependencies; bump version

--- index.d.ts
+++ index.d.ts
@@ -5,5 +5,5 @@ export declare const has_body: (req: restify.Request, res: restify.Response, nex
 export declare const jsonSchemaErrorParser: (body_is: MultiResult) => CustomJsonError | undefined;
 export declare const mk_valid_body_mw: (json_schema: JsonSchema, to_res?: boolean) => (request: restify.Request, res: restify.Response, next: restify.Next) => void;
 export declare const mk_valid_body_mw_ignore: (json_schema: JsonSchema, ignore: string[]) => (req: restify.Request, res: restify.Response, next: restify.Next) => void;
-export declare const jsonSchemaNamedArrayOf: (json_schema: tv4.JsonSchema, type_name?: string | undefined, type_plural?: string | undefined) => tv4.JsonSchema;
+export declare const jsonSchemaNamedArrayOf: (json_schema: tv4.JsonSchema, type_name?: string, type_plural?: string) => tv4.JsonSchema;
 export declare const remove_from_body: (keys: string[]) => (request: restify.Request, res: restify.Response, next: restify.Next) => void;
